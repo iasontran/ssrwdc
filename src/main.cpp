411d7c3f03fe5de348ee91056c1855fd5ff532cb
Anthony Vega <vegant42@email.arizona.edu>
2018-02-01 15:33:46 -0700

    SD library and SD_Reader files created

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+// Author:
+// Net ID:
+// Date:
+// Assignment:     Lab 4
+//
+// Description: This file contains a programmatic overall description of the
+// program. It should never contain assignments to special function registers
+// for the exception key one-line code such as checking the state of the pin.
+//
+// Requirements:
+//----------------------------------------------------------------------//
+
+#include <avr/io.h>
+#include <SD_Reader.H>
+#include "..\lib\SdFat\src\SdFat.h"
+
+
+int main(){
+
+
+  while(1){
+
+  }
+
+  return 0;
+}
