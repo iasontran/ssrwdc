f8dae2132a69e68adf27b476fc80149af2d32a0c
Anthony Vega <vegant42@email.arizona.edu>
2018-02-01 16:28:10 -0700

    Template for sdREader

--- src/SD_Reader.h
+++ src/SD_Reader.h
@@ -7,3 +7,15 @@
 //
 // Requirements:
 //----------------------------------------------------------------------//
+#ifndef SD_Reader_H
+#define SD_Reader_H
+
+
+
+#include <avr/io.h>
+#include <string.h>
+using namespace std;
+void initiateSDReader();
+char* getKey();
+
+#endif
